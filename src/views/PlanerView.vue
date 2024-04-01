3ba2070547caf31a3e3a2f07e54ac692b37d6a64
SpaceShaman <spaceshaman@tuta.io>
2024-04-01 11:17:57 +0200

    feat(package.json): add interactjs library as a dependency to enable interactive features in the application feat(App.vue): update RouterLink in the navigation to point to the 'planer' route instead of 'home' feat(router/index.ts): update route to use PlanerView component instead of HomeView feat(views): add PlanerView component and remove HomeView component to reflect the new structure and content for the 'planer' page

---
+++ src/views/PlanerView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import interact from 'interactjs'
+
+const position = { x: 0, y: 0 }
+
+interact('.plant')
+  .draggable({
+    listeners: {
+      start(event) {
+        console.log(event.type, event.target)
+      },
+      move(event) {
+        position.x += event.dx
+        position.y += event.dy
+
+        event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
+      }
+    }
+  })
+  .styleCursor(false)
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+    <img alt="Carrot" class="plant" src="@/assets/plants/carrot.svg" width="100" />
+  </main>
+</template>
+
+<style scoped>
+.plant {
+  width: 100px;
+  touch-action: none;
+  user-select: none;
+  cursor: grab;
+  padding: 100px;
+  border-radius: 50%;
+  background-color: rgba(0, 255, 13, 0.5);
+}
+</style>
