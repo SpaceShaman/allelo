c05e5c7e9c5e879708c55b19b345e197302d90d2
SpaceShaman <spaceshaman@tuta.io>
2024-04-22 19:09:39 +0200

    style(Selected.vue): add density="compact" and slim attributes to v-list component for a more compact layout feat(Selected.vue): add v-list-subheader "Selected" for better organization and clarity feat(Selected.vue): update v-list-item title template to display group name and number of plants feat(Selected.vue): update v-list-item activator template to include slim attribute for a more streamlined appearance feat(Selected.vue): update v-tabs component with fixed-tabs attribute and customize tab values and colors feat(Selected.vue): update v-window-item values to match tab values for consistency style(Selected.vue): add width and padding styles to improve layout and readability of the component

--- src/components/Selected.vue
+++ src/components/Selected.vue
@@ -38,7 +38,8 @@ const tab = ref<string>("positive");
       :text="`${viewport.mouse.pressed} ${viewport.mouse.button} ${viewport.mouse.target?.id} \n${viewport.mouse.moveX} x ${viewport.mouse.moveY}`"
       variant="text"
     /> -->
-    <v-list v-model:opened="open">
+    <v-list v-model:opened="open" density="compact" slim>
+      <v-list-subheader>Selected</v-list-subheader>
       <v-list-group
         v-for="[group, plants] of Object.entries(grupedPlants)"
         :key="group"
@@ -51,11 +52,13 @@ const tab = ref<string>("positive");
             <template v-slot:prepend>
               <PlantIcon :name="group" />
             </template>
-            <template v-slot:title
-              ><h2>
-                {{ group.charAt(0).toUpperCase() + group.slice(1) }}s
-              </h2></template
-            >
+            <template v-slot:title>
+              <h3>
+                {{ group.charAt(0).toUpperCase() + group.slice(1) }}s ({{
+                  plants.length
+                }})
+              </h3>
+            </template>
           </v-list-item>
         </template>
         <v-list-group
@@ -64,7 +67,7 @@ const tab = ref<string>("positive");
           :value="`${group}-${id}`"
         >
           <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props">
+            <v-list-item v-bind="props" slim>
               <template v-slot:prepend>
                 <PlantIcon :name="group" />
               </template>
@@ -78,17 +81,17 @@ const tab = ref<string>("positive");
           </template>
           <v-list-item>
             <template v-slot:subtitle>
-              <v-tabs v-model="tab">
-                <v-tab value="positive">Positive</v-tab>
-                <v-tab value="negative">Negative</v-tab>
+              <v-tabs v-model="tab" fixed-tabs>
+                <v-tab value="friends" color="green">Friends</v-tab>
+                <v-tab value="enemys" color="red">Enemys</v-tab>
               </v-tabs>
             </template>
 
             <v-card-text>
               <v-window v-model="tab">
-                <v-window-item value="positive"> Positive </v-window-item>
+                <v-window-item value="friends"> Positive </v-window-item>
 
-                <v-window-item value="negative"> negative </v-window-item>
+                <v-window-item value="enemys"> enemys </v-window-item>
               </v-window>
             </v-card-text>
           </v-list-item>
@@ -103,9 +106,16 @@ const tab = ref<string>("positive");
   position: fixed;
   right: 0;
   margin: 20px;
+  width: 300px;
   max-height: calc(100vh - 40px);
   z-index: 2;
   touch-action: none;
   user-select: none;
+}
+.v-list-group__items .v-list-item {
+  padding-left: 16px !important;
+}
+.v-list-item {
+  padding-left: 16px !important;
 }
 </style>
