1a51b267102b20298f000fbd04d13d5072b7e5a9
SpaceShaman <spaceshaman@tuta.io>
2024-04-28 12:01:15 +0200

    refactor(Toolbar.vue): extract toTitle function to improve code readability and reusability style(Toolbar.vue): improve button layout and add tooltips for better user experience

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -5,6 +5,10 @@ import { ref, watch } from "vue";
 
 const toolbar = toolbarStore();
 
+function toTitle(text: string) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
 watch(
   () => toolbar.selected,
   (value: string, oldValue: string) => {
@@ -16,9 +20,24 @@ const tab = ref<string>();
 
 <template>
   <v-btn-toggle v-model="toolbar.selected" mandatory elevation="2">
-    <v-btn icon="mdi-cursor-default" value="default"></v-btn>
-    <v-btn icon="mdi-select" value="select"><v-icon>mdi-select</v-icon></v-btn>
-    <v-btn value="growing-bed"><PlantIcon name="growing-bed" /></v-btn>
+    <v-btn icon="mdi-cursor-default" value="default">
+      <v-icon />
+      <v-tooltip activator="parent" location="bottom"
+        ><h2>Default</h2></v-tooltip
+      >
+    </v-btn>
+    <v-btn icon="mdi-select" value="select">
+      <v-icon />
+      <v-tooltip activator="parent" location="bottom"
+        ><h2>Selection</h2></v-tooltip
+      >
+    </v-btn>
+    <v-btn value="growing-bed">
+      <PlantIcon name="growing-bed" />
+      <v-tooltip activator="parent" location="bottom"
+        ><h2>Growing Bed</h2></v-tooltip
+      >
+    </v-btn>
     <v-btn
       v-for="[name, plant] in Object.entries(plants)"
       :key="name"
@@ -26,13 +45,19 @@ const tab = ref<string>();
     >
       <PlantIcon :name="name" />
       <v-tooltip activator="parent" location="bottom">
-        <h2>{{ name.charAt(0).toUpperCase() + name.slice(1) }}</h2>
+        <h2>{{ toTitle(name) }}</h2>
         <v-row>
           <v-col>
             <h3 class="text-green">Friends</h3>
+            <p v-for="friend in plant.friends" :key="friend">
+              {{ toTitle(friend) }}
+            </p>
           </v-col>
           <v-col>
             <h3 class="text-red">Enemies</h3>
+            <p v-for="enemy in plant.enemies" :key="enemy">
+              {{ toTitle(enemy) }}
+            </p>
           </v-col>
         </v-row>
       </v-tooltip>
