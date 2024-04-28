802c3fea970cc60eb81cde550e4d4889583423e8
SpaceShaman <spaceshaman@tuta.io>
2024-04-28 13:51:31 +0200

    feat(Toolbar.vue): add PlantIcon component and onMenuClick function to handle right-click events on plant items in the toolbar feat(Toolbar.vue): refactor v-for loop to display plant items in a v-menu component for better user experience

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -2,6 +2,7 @@
 import plants from "@/plants";
 import { toolbarStore } from "@/stores";
 import { ref, watch } from "vue";
+import PlantIcon from "./PlantIcon.vue";
 
 const toolbar = toolbarStore();
 
@@ -16,6 +17,12 @@ watch(
   }
 );
 const tab = ref<string>();
+
+function onMenuClick(event: MouseEvent, props: any) {
+  if (event.button === 2) {
+    props.onClick(event);
+  }
+}
 </script>
 
 <template>
@@ -38,50 +45,41 @@ const tab = ref<string>();
         ><h2>Growing Bed</h2></v-tooltip
       >
     </v-btn>
-    <v-btn
-      v-for="[name, plant] in Object.entries(plants)"
-      :key="name"
-      :value="`plant-${name}`"
-    >
-      <PlantIcon :name="name" />
-      <v-tooltip activator="parent" location="bottom">
-        <h2>{{ toTitle(name) }}</h2>
-        <v-row>
-          <v-col>
-            <h3 class="text-green">Friends</h3>
-            <p v-for="friend in plant.friends" :key="friend">
-              {{ toTitle(friend) }}
-            </p>
-          </v-col>
-          <v-col>
-            <h3 class="text-red">Enemies</h3>
-            <p v-for="enemy in plant.enemies" :key="enemy">
-              {{ toTitle(enemy) }}
-            </p>
-          </v-col>
-        </v-row>
-      </v-tooltip>
-    </v-btn>
-    <!-- <v-menu>
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props">
-          <v-icon style="--v-icon-size-multiplier: 1.5">
-            <v-img :src="getPlantSvg('strawberry')"></v-img>
-          </v-icon>
+    <v-menu v-for="[name, plant] in Object.entries(plants)" :key="name">
+      <template v-slot:activator="{ isActive, props }">
+        <v-btn
+          @contextmenu.prevent="onMenuClick($event, props)"
+          :value="`plant-${name}`"
+        >
+          <PlantIcon :name="name" />
+          <v-tooltip activator="parent" location="bottom" v-if="!isActive">
+            <h2>{{ toTitle(name) }}</h2>
+            <v-row>
+              <v-col>
+                <h3 class="text-green">Friends</h3>
+                <p v-for="friend in plant.friends" :key="friend">
+                  {{ toTitle(friend) }}
+                </p>
+              </v-col>
+              <v-col>
+                <h3 class="text-red">Enemies</h3>
+                <p v-for="enemy in plant.enemies" :key="enemy">
+                  {{ toTitle(enemy) }}
+                </p>
+              </v-col>
+            </v-row>
+          </v-tooltip>
         </v-btn>
       </template>
       <v-list>
-        <v-list-item
-          v-for="plant in plants"
-          :key="plant"
-          :value="plant"
-        >
-          <v-icon style="--v-icon-size-multiplier: 1.5">
-            <v-img :src="getPlantSvg(plant)"></v-img>
-          </v-icon>
+        <v-list-item value="carrot">
+          <PlantIcon name="carrot" />
+        </v-list-item>
+        <v-list-item value="broccoli">
+          <PlantIcon name="broccoli" />
         </v-list-item>
       </v-list>
-    </v-menu> -->
+    </v-menu>
     <ColorModeButton />
   </v-btn-toggle>
 </template>
